4aa14f7ac69dacaf12906638991896e344303292
Oleg <crashyourselfcontrol@gmail.com>
2018-02-25 17:10:08 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
-module.exports = solveEquation;
-// console.log('solveEquation(\'264 * x^2 + 10632072 * x - 921283858117392\'): ', solveEquation('264 * x^2 + 10632072 * x - 921283858117392'));
-function solveEquation(equation) {
+module.exports = function solveEquation(equation) {
   let a = getA(equation);
   let b = getB(equation);
   let c = getC(equation);
   let D = getD(a, b, c);
-  // console.log('a: ', a);
-  // console.log('b: ', b);
-  // console.log('c: ', c);
-  // console.log('D: ', D);
 
   if (D < 0) {
-    // console.log(' new Array(0): ',  new Array(0));
     return new Array(0);
   } else if (D === 0) {
-    // console.log('new Array(Math.round(-b / (2 * a))): ', new Array().push(Math.round(-b / (2 * a))));
     return [Math.round(-b / (2 * a))];
   } else if (D > 0) {
-    // console.log('new Array(getX1(a, b, D), getX2(a, b, D)): ', new Array(getX1(a, b, D), getX2(a, b, D)));
-    return new Array(getX1(a, b, D), getX2(a, b, D)).sort();
+    return [getX1(a, b, D), getX2(a, b, D)].sort((a, b) => a -b);
   }
 
   function getA(equationString) {
@@ -27,9 +18,9 @@ function solveEquation(equation) {
     : -equationString.split(' ')[1];
   }
   function getB(equationString) {
+    let equationArray = equationString.split(' ');
     let indexB;
-    let b = equationString
-      .split(' ')
+    let b = equationArray
       .filter((item, i) => {
         if (i > 1 && !isNaN(+item) && !indexB) {
           indexB = i;
@@ -37,7 +28,8 @@ function solveEquation(equation) {
         }
         return false;
       })[0];
-    let symbol = equationString.split(' ')[indexB - 1];
+    let symbol = equationArray[indexB - 1];
+
     if (symbol === '-') {
       return -b;
     }
